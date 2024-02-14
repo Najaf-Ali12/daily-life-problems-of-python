dc6b7ecf895b75b14cf79eeaabdd502f7a062772
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-02-14 06:24:38 +0500

    Add files via upload

---
+++ zong packages.py
@@ -0,0 +1,434 @@
+def package2():
+    print("1.mini")
+    print("2.weekly")
+    print("3.Monthly")
+    opt=int(input("enter any number that you want to do "))
+    if opt==1:
+        print("1.1GB,On-net 3Day Bundle @Rs.87")
+        print("2.Daily shandaar @ Rs.17")
+        print("3.1GB,Unlimited On-net 2 HOUR Non-stop @Rs.14")
+        opt=int(input("enter any number that you want to do"))
+        if opt==1:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 1 GB,1000 onnet mins,10 other operator mins, for 3 days @ Rs.100 load")
+            if opt==2:
+                if amount>=100:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==2:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 50MB,1440 onnet mins,800 SMS, for 1 day @ Rs.19.5 load")
+            if opt==2:
+                if amount>=20:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==3:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 50MB,1440 onnet mins,800 SMS, for 1 day @ Rs.19.5 load")
+            if opt==2:
+                if amount>=20:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+    if opt==2:
+        print("1. 80 GB,1000 Offnet @Rs.435")
+        print("2. 60 GB,600 Offnet @Rs.418")
+        print("3. 30 GB,180 Offnet @Rs.383")
+        print("4. 12 GB,100 Offnet @Rs.322")
+        print("5. 4 GB,60 Offnet @Rs.235")
+        opt=int(input("enter any number from above that you want to do"))
+        if opt==1:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 80 GB,10000 onnet mins,1000 other operator mins,10000 SMS  for 7 days @ Rs.500 load")
+            if opt==2:
+                if amount>=500:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==2:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 60 GB,6000 onnet mins,600 other operator mins,6000 SMS  for 7 days @ Rs.480 load")
+            if opt==2:
+                if amount>=480:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==3:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 30 GB,5000 onnet mins,180 other operator mins,5000 SMS  for 7 days @ Rs.440 load")
+            if opt==2:
+                if amount>=440:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==4:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 12 GB,5000 onnet mins,100 other operator mins,5000 SMS  for 7 days @ Rs.370 load")
+            if opt==2:
+                if amount>=370:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==5:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 4GB,5000 onnet mins,60 other operator mins,5000 SMS  for 7 days @ Rs.270 load")
+            if opt==2:
+                if amount>=270:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+    if opt==3:
+        print("1.200GB,2000 Offnet @Rs.1392")
+        print("2.100GB,1000 Offnet @Rs.1305")
+        print("3.30GB, 600 Offnet @Rs.957")
+        print("4.20GB, 500 Offnet @Rs.869")
+        print("5.12GB, 350 Offnet @Rs.783")
+        print("6.5GB,  200 Offnet @Rs.696")
+        if opt==1:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 200GB,20000 onnet mins,200 other operator mins,10000 SMS  for 30 days @ Rs.1,600 load")
+            if opt==2:
+                if amount>=1600:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==2:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 100GB,10000 onnet mins,1000 other operator mins,20000 SMS  for 30 days @ Rs.1,600 load")
+            if opt==2:
+                if amount>=1500:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==3:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 30GB,5000 onnet mins,600 other operator mins,5000 SMS  for 30 days @ Rs.1,100 load")
+            if opt==2:
+                if amount>=1100:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==4:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 20GB,5000 onnet mins,500 other operator mins,5000 SMS  for 30 days @ Rs.999 load")
+            if opt==2:
+                if amount>=1000:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==5:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 10GB,5000 onnet mins,350 other operator mins,5000 SMS  for 30 days @ Rs.900 load")
+            if opt==2:
+                if amount>=900:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==6:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 5GB,5000 onnet mins,200 other operator mins,5000 SMS  for 30 days @ Rs.800 load")
+            if opt==2:
+                if amount>=800:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+def package3():
+    print("1.weekly")
+    print("2.Monthly")
+    opt=int(input("enter any number that you want to do "))
+    if opt==1:
+        print("1.80GB, 1000 Offnet @Rs.435")
+        print("2.20GB, 100 Offnet @Rs.366")
+        print("3.12GB, 100 Offnet @Rs.305")
+        print("4.4GB, 60 Offnet @Rs.235")
+        if opt==1:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 80GB,10000 onnet mins,1000 other operator" 
+                      "mins,10000 SMS  for 7 days @ Rs.500 load")
+            if opt==2:
+                if amount>=500:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==2:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 20GB,5000 onnet mins,100 other operator mins,"
+                      "5000 SMS  for 7days @ Rs.420 load")
+            if opt==2:
+                if amount>=420:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==3:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 12GB,5000onnet mins,100 other operator mins,"
+                      "5000 SMS  for 7days @ Rs.370 load")
+            if opt==2:
+                if amount>=370:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==4:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 4GB,5000 onnet mins,60 other operator mins,"
+                      "5000 SMS  for 7 days @ Rs.270 load")
+            if opt==2:
+                if amount>=270:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+    if opt==2:
+        print("1.200GB, 2000 Offnet @Rs.1392")
+        print("2.100GB, 1000 Offnet @Rs.1305")
+        print("3.40GB monthly @Rs.870")
+        print("4.20 1000 monthly @Rs.609")
+        print("5.5GB monthly @Rs.348")
+        if opt==1:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 200GB,20000 onnet mins,2000 other operator"
+                       "mins,20000 SMS  for 30days @ Rs.1600 load")
+            if opt==2:
+                if amount>=1600:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+        if opt==2:
+            print("1.Information")
+            print("2.Subscribe")
+            opt=int(input("enter any number from above that you want to do"))
+            if opt==1:
+                print("Enjoy 100GB,10000 onnet mins,1000 other operator"
+                       "mins,10000 SMS  for 30days @ Rs.1500 load")
+            if opt==2:
+                if amount>=1500:
+                    print("You have successfully subscribed")
+                else:
+                    print("Recharge required")
+def package4():
+    print("1.2000 Onnet,300 Offnet Weekly @Rs.122")
+    print("2.1000 Onnet,Monthly @Rs.174")
+    print("3.2000 Onnet,100 Offnet Monthly @Rs.261")
+    print("4.Allnet Monthly @Rs.348")
+    opt=int(input("enter any number that you want to do "))
+def package5():
+    print("1.mini")
+    print("2.weekly")
+    print("3.Monthly")
+    opt=int(input("enter any number that you want to do "))
+    if opt==1:
+        print(" ")
+def package6():
+    print("1.Mini")
+    print("2.weekly")
+    opt=int(input("enter any number that you want to do "))
+    if opt==1:
+        print(" ")
+def package7():
+    print("1.Monthly SMS @Rs.87")
+    print("2.Weekly SMS @Rs.31")
+    print("3.Daily SMS @Rs.9")
+    opt=int(input("enter any number that you want to do "))
+    if opt==1:
+        print("1.Subscribe")
+        if opt==1:
+            if amount>90:
+                print("you have subscribed successfully")
+            else:
+                print("Recharge required")
+    if opt==2:
+        print("1.Subscribe")
+        if opt==1:
+            if amount>40:
+                print("you have subscribed successfully")
+            else:
+                print("Recharge required")
+    if opt==3:
+        print("1.Subscribe")
+        if opt==1:
+            if amount>15:
+                print("you have subscribed successfully")
+            else:
+                print("Recharge required")
+def package8():
+    print("1.Zong Cinema@Rs.6+T")
+    print("2.Zong Mobile TV@Rs.6+T")
+    print("3.Zong Mobile Theater@Rs.3+T")
+    print("4.Zong Internet World@Rs.3+T")
+    print("5.Zong Play Store@Rs.5+T")
+    opt=int(input("enter any number that you want to do "))
+    if opt==1:
+        if amount>=10:
+            print("Please wait,your request has been processed")
+    if opt==2:
+        if amount>=10:
+            print("Please wait,your request has been processed")
+    if opt==3:
+        if amount>=6:
+            print("Please wait,your request has been processed")
+    if opt==4:
+        if amount>=6:
+            print("Please wait,your request has been processed")
+    if opt==5:
+        if amount>=9:
+            print("Please wait,your request has been processed")
+
+    
+
+
+     
+    
+def package1():
+    print("1.200GB,2000 Offnet@Rs1392 30Day")
+    print("2.100GB,1000 Offnet@Rs1305 30Day")
+    print("3.80GB,1000 Offnet@Rs435 7Day")
+    print("4.20GB Social,100 Offnet@Rs305 7Day")
+    print("5.12GB,100 Offnet@Rs322 7Day")
+    print("6.Back")
+    opt=int(input("enter any number from above that you want to do"))
+    if opt==1:
+        print("1.Information")
+        print("2.Subscribe")
+        opt=int(input("enter any number from above that you want to do"))
+        if opt==1:
+            print("Enjoy 20 GB,20000 onnet mins,2000 other operator mins,20000 SMS for 30 days @ Rs.1,600 load")
+        if opt==2:
+            if amount>=1600:
+                print("You have successfully subscribed")
+    if opt==2:
+        print("1.Information")
+        print("2.Subscribe")
+        opt=int(input("enter any number from above that you want to do"))
+        if opt==1:
+            print("Enjoy 10 GB,10000 onnet mins,1000 other operator mins,10000 SMS for 30 days @ Rs.1,500 load")
+        if opt==2:
+            if amount>=1500:
+                print("You have successfully subscribed")
+    if opt==3:
+        print("1.Information")
+        print("2.Subscribe")
+        opt=int(input("enter any number from above that you want to do"))
+        if opt==1:
+            print("Enjoy 80 GB,10000 onnet mins,1000 other operator mins,10000 SMS for 7 days @ Rs.500 load")
+        if opt==2:
+            if amount>=500:
+                print("You have successfully subscribed")
+    if opt==4:
+        print("1.Information")
+        print("2.Subscribe")
+        opt=int(input("enter any number from above that you want to do"))
+        if opt==1:
+            print("Enjoy 20 GB whatsapp,Facebook,Imo and Yt,500 onnet mins,100 other operator mins,500 SMS for 30 days @ Rs.350 load")
+        if opt==2:
+            if amount>=350:
+                print("You have successfully subscribed")
+    if opt==5:
+        print("1.Information")
+        print("2.Subscribe")
+        opt=int(input("enter any number from above that you want to do"))
+        if opt==1:
+            print("Enjoy 12 GB,5000 onnet mins,100 other operator mins,5000 SMS for 7 days @ Rs.370 load")
+        if opt==2:
+            if amount>=370:
+                print("You have successfully subscribed")
+    if opt==6:
+        packages()
+
+
+
+         
+    
+
+def packages():
+    while True:
+        print("dial *6464# for packages of zong if you want")
+        print("1.Top Packages")
+        print("2.All-in-one-Package")
+        print("3.Data Package")
+        print("4.voice")
+        print("5.Social Package")
+        print("6.Stay-At-Home")
+        print("7.SMS")
+        print("8.TV,Videos,Music")
+        print("9.Exit")
+        choice=int(input("enter any number that you want from above"))
+        if choice==1:
+            package1()
+        elif choice==2:
+            package2()
+        elif choice==3:
+            package3()
+        elif choice==4:
+            package4()
+        elif choice==5:
+            package5()
+        elif choice==6:
+            package6()
+        elif choice==7:
+            package7()
+        elif choice==8:
+            package8()
+        elif choice==9:
+            break
+        else:
+            print("INVALID CHOICE")  
+amount=int(input("enter the amount present in your account"))
+packages()
